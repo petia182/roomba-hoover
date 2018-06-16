c1538416dfc9d666bd6fef39c75b99c57b81b84a
Petya Nikolova <petia_182@yahoo.com>
2018-06-16 20:22:57 +0100

    adding roomba-settings component and passing props

---
+++ src/components/roomba-settings.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+class RoombaSettings extends React.Component {
+	render() {
+		const {
+			robotYCoordinates,
+			robotXCoordinates,
+			setRobotXCoordinates,
+			setRobotYCoordinates
+		} = this.props;
+
+		return (
+			<React.Fragment>
+				<div className="roomba-coordinates">
+					Roomba coordinates
+					<p>X: {robotXCoordinates}</p>
+					<p>Y: {robotYCoordinates}</p>
+				</div>
+				<div className="form-group">
+					<label htmlFor="">Set X coordinate (between 0 and 5):</label>
+					<input
+						onChange={setRobotXCoordinates}
+						type="number"
+						min="0"
+						max="5"
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="">Set Y coordinate (between 0 and 5):</label>
+					<input
+						onChange={setRobotYCoordinates}
+						type="number"
+						min="0"
+						max="5"
+					/>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
+export default RoombaSettings;

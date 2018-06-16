20294f6b537ffee77fc7dcad9accfb6da7dee40f
Petya Nikolova <petia_182@yahoo.com>
2018-06-16 17:28:21 +0100

    adding roomdimentions; adding user input option for updating the room dimensions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 
 class App extends Component {
+  state = {
+    roomDimensions: {
+      width: 600,
+      height: 600,
+    },
+    robotCoordinates: {
+      x: 0,
+      y: 0,
+    },
+    dirtCoordinates: {
+      x: 0,
+      y: 0,
+    },
+  }
+
+  setRoomWidthDimensions = (e) => {
+    this.setState({
+      roomDimensions: {
+        width: e.target.value,
+        height: this.state.roomDimensions.height
+      }
+    })
+  }
+
+  setRoomHeightDimensions = (e) => {
+    this.setState({
+      roomDimensions: {
+        height: e.target.value,
+        width: this.state.roomDimensions.width
+      }
+    })
+  }
+
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+			<div className="container">
+        <form action="">
+          Room Dimensions (number)
+          <br />
+          <label htmlFor="room-width-dimensions">Set width</label>
+          <input onChange={this.setRoomWidthDimensions} id="room-width-dimensions" type="number"/>
+          <label htmlFor="room-height-dimensions">Set length</label>
+          <input onChange={this.setRoomHeightDimensions} id="room-height-dimensions" type="number"/>
+        </form>
+        <div>
+          <p>Room dimensions</p>
+          <p>Width: {this.state.roomDimensions.width}px</p>
+          <p>Length: {this.state.roomDimensions.height}px</p>
+        </div>
+				<div style={{ width: parseInt(this.state.roomDimensions.width), height: parseInt(this.state.roomDimensions.height)  }} className="coordinate-system">
+				</div>
 			</div>
 		);
 	}

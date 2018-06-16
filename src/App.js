dc7b9a3c17d4d8128e2817ee2bd22a6fea4900b5
Petya Nikolova <petia_182@yahoo.com>
2018-06-16 19:03:19 +0100

    adding initial robot coordinates; adding input for setting initial robot X and Y coordinates

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import "./App.scss";
 class App extends Component {
 	state = {
 		roomDimensions: {
-			width: 600,
-			height: 600
+			width: 400,
+			height: 400
 		},
 		robotCoordinates: {
 			x: 0,
 			y: 0
 		},
 		dirtCoordinates: {
-			x: 0,
-			y: 0
+			x: 1,
+			y: 1
 		}
 	};
 
@@ -32,6 +32,24 @@ class App extends Component {
 			roomDimensions: {
 				height: e.target.value,
 				width: this.state.roomDimensions.width
+			}
+		});
+	};
+
+	setRobotXCoordinates = e => {
+		this.setState({
+			robotCoordinates: {
+				x: (e.target.value - 1) * this.state.roomDimensions.width / 5,
+				y: this.state.robotCoordinates.y
+			}
+		});
+	};
+
+	setRobotYCoordinates = e => {
+		this.setState({
+			robotCoordinates: {
+				y: (e.target.value - 1) * this.state.roomDimensions.width / 5,
+				x: this.state.robotCoordinates.x
 			}
 		});
 	};
@@ -56,26 +74,56 @@ class App extends Component {
 	render() {
 		return (
 			<div className="container">
-				<form action="">
-					Room Dimensions (number)
+				<div className="settings">
+					<div className="room-coordinates">
+						<p>Current Room dimensions</p>
+						<p>Width: {this.state.roomDimensions.width}px</p>
+						<p>Length: {this.state.roomDimensions.height}px</p>
+					</div>
+					Set Room Dimensions (number)
 					<br />
-					<label htmlFor="room-width-dimensions">Set width</label>
-					<input
-						onChange={this.setRoomWidthDimensions}
-						id="room-width-dimensions"
-						type="number"
-					/>
-					<label htmlFor="room-height-dimensions">Set length</label>
-					<input
-						onChange={this.setRoomHeightDimensions}
-						id="room-height-dimensions"
-						type="number"
-					/>
-				</form>
-				<div>
-					<p>Room dimensions</p>
-					<p>Width: {this.state.roomDimensions.width}px</p>
-					<p>Length: {this.state.roomDimensions.height}px</p>
+					<form action="">
+						<div className="form-group">
+							<label htmlFor="room-width-dimensions">Set width</label>
+							<input
+								onChange={this.setRoomWidthDimensions}
+								id="room-width-dimensions"
+								type="number"
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="room-height-dimensions">Set length</label>
+							<input
+								onChange={this.setRoomHeightDimensions}
+								id="room-height-dimensions"
+								type="number"
+							/>
+						</div>
+						<hr />
+						<div className="roomba-coordinates">
+							Roomba coordinates
+							<p>X: {this.state.robotCoordinates.x}</p>
+							<p>Y: {this.state.robotCoordinates.y}</p>
+						</div>
+						<div className="form-group">
+							<label htmlFor="">Set X coordinate (between 0 and 5):</label>
+							<input
+								onChange={this.setRobotXCoordinates}
+								type="number"
+								min="0"
+								max="5"
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="">Set Y coordinate (between 0 and 5):</label>
+							<input
+								onChange={this.setRobotYCoordinates}
+								type="number"
+								min="0"
+								max="5"
+							/>
+						</div>
+					</form>
 				</div>
 				<div
 					style={{
@@ -85,6 +133,19 @@ class App extends Component {
 					className="coordinate-system"
 				>
 					{this.renderBlock()}
+					<div
+						style={{
+							width: this.state.roomDimensions.width / 5,
+							height: this.state.roomDimensions.height / 5,
+							bottom: this.state.robotCoordinates.y,
+							left: this.state.robotCoordinates.x
+						}}
+						className="roomba"
+					>
+						Roomba
+					</div>
+					<p className="x-axis">X axis</p>
+					<p className="y-axis">Y axis</p>
 				</div>
 			</div>
 		);

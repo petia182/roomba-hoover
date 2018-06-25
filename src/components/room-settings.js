bc2abdf687b9ce0862946d3d603ca0cfe3a4e523
Petya Nikolova <petia_182@yahoo.com>
2018-06-25 22:12:00 +0100

    updating instructions

--- src/components/room-settings.js
+++ src/components/room-settings.js
@@ -1,54 +1,54 @@
-import React from 'react';
+import React from "react";
 
 class RoomSettings extends React.Component {
-  render() {
-    const {
-      roomDimensionsWidth,
-      roomDimensionsHeight,
-      setRoomWidthDimensions,
-      setRoomHeightDimensions,
-    } = this.props;
-    return (
-      <React.Fragment>
-        <div className="instructions">
-          <h1>Instructions</h1>
-          <ol>
-            {/* <li>(Optional) Set the coordinates of the room (min 0, max 10)</li>
+	render() {
+		const {
+			roomDimensionsWidth,
+			roomDimensionsHeight,
+			setRoomWidthDimensions,
+			setRoomHeightDimensions
+		} = this.props;
+		return (
+			<React.Fragment>
+				<div className="instructions">
+					<h1>Instructions</h1>
+					<ol>
+						{/* <li>(Optional) Set the coordinates of the room (min 0, max 10)</li>
             <li>
               (Optional) Set the starting point of the roomba (min 0, max 9)
             </li> */}
-            <li>Set the coordinates of 0 or more dirt patches</li>
-            <li>Move the roomba using the arrow keys or (A,W,S,D)</li>
-          </ol>
-        </div>
-        <div className="room-coordinates">
-          <i>Current Room dimensions</i>
-          <p>
-            <span>X: {roomDimensionsWidth}</span>
-            <span>Y: {roomDimensionsHeight}</span>
-          </p>
-        </div>
-        <div className="room-settings">
-          Set Room Dimensions (number)
-          <br />
-          <label htmlFor="room-width-dimensions">X:</label>
-          <input
-            onChange={setRoomWidthDimensions}
-            type="number"
-            min="1"
-            max="10"
-          />
-          <label htmlFor="room-height-dimensions">Y:</label>
-          <input
-            onChange={setRoomHeightDimensions}
-            type="number"
-            min="1"
-            max="10"
-          />
-        </div>
-      </React.Fragment>
-    );
-  }
+						<li>Set the coordinates of 0 or more dirt patches</li>
+						<li>Move the roomba using the arrow keys or (A,W,S,D)</li>
+					</ol>
+				</div>
+				<div className="room-coordinates">
+					<i>Current room dimensions</i>
+					<p>
+						<span>X: {roomDimensionsWidth}</span>
+						<span>Y: {roomDimensionsHeight}</span>
+					</p>
+				</div>
+				<div className="room-settings">
+					Update room dimensions
+					<br />
+					<label htmlFor="room-width-dimensions">X:</label>
+					<input
+						onChange={setRoomWidthDimensions}
+						type="number"
+						min="1"
+						max="10"
+					/>
+					<label htmlFor="room-height-dimensions">Y:</label>
+					<input
+						onChange={setRoomHeightDimensions}
+						type="number"
+						min="1"
+						max="10"
+					/>
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
 export default RoomSettings;
--- src/components/roomba-settings.js
+++ src/components/roomba-settings.js
@@ -1,45 +1,45 @@
-import React from 'react';
+import React from "react";
 
 class RoombaSettings extends React.Component {
-  render() {
-    const {
-      robotYCoordinates,
-      robotXCoordinates,
-      setRobotXCoordinates,
-      setRobotYCoordinates,
-      blockSize,
-    } = this.props;
+	render() {
+		const {
+			robotYCoordinates,
+			robotXCoordinates,
+			setRobotXCoordinates,
+			setRobotYCoordinates,
+			blockSize
+		} = this.props;
 
-    return (
-      <React.Fragment>
-        <div className="roomba-coordinates">
-          <i>Current Roomba coordinates</i>
-          <p>
-            <span>X: {robotXCoordinates / blockSize}</span>
-            <span>Y: {robotYCoordinates / blockSize}</span>
-          </p>
-        </div>
-        <div className="roomba-settings">
-          Set Roomba starting point
-          <br />
-          <label htmlFor="">X:</label>
-          <input
-            onChange={setRobotXCoordinates}
-            type="number"
-            min="0"
-            max="9"
-          />
-          <label htmlFor="">Y:</label>
-          <input
-            onChange={setRobotYCoordinates}
-            type="number"
-            min="0"
-            max="9"
-          />
-        </div>
-      </React.Fragment>
-    );
-  }
+		return (
+			<React.Fragment>
+				<div className="roomba-coordinates">
+					<i>Current Roomba coordinates</i>
+					<p>
+						<span>X: {robotXCoordinates / blockSize}</span>
+						<span>Y: {robotYCoordinates / blockSize}</span>
+					</p>
+				</div>
+				<div className="roomba-settings">
+					Update Roomba starting point
+					<br />
+					<label htmlFor="">X:</label>
+					<input
+						onChange={setRobotXCoordinates}
+						type="number"
+						min="0"
+						max="9"
+					/>
+					<label htmlFor="">Y:</label>
+					<input
+						onChange={setRobotYCoordinates}
+						type="number"
+						min="0"
+						max="9"
+					/>
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
 export default RoombaSettings;
